226848e26783eec6f007b5441128cf5ac7350eef
TSuxrobbek <119966808+TSuxrobbek@users.noreply.github.com>
2023-03-14 12:45:43 +0300

    Yangi fayllar yuklandi
    11-dars qo'shildi

---
+++ python-darslari 11-dars/main11.py
@@ -0,0 +1,84 @@
+# 11-Dars if-agar else-aks holda elif
+# Sana 20/02/2023
+# Muallif: Toshtemirov Sukhrobbek
+# son = -1
+# if son < 0:
+#     print("Manfiy son")
+# else:
+#     print("Musmat son")
+# def sukhrob():
+
+#     yosh = int(input("Yoshingiz nechchida?"))
+#     if yosh <= 4:
+#         print("Sizga kirish bepul")
+#     elif yosh <= 12:
+#         print("Sizga kirish 5000 so'm")
+#     elif yosh <= 20:
+#         print("Sizga kirish 10000 so'm")
+#     else:
+#         print("Sizga kirish 15000 so'm")
+
+
+# sukhrob()
+
+
+# yosh = int(input("Yoshingiz nechchida?"))
+# if yosh <= 4:
+#     print("Sizga kirish bepul")
+# elif yosh <= 12:
+#     print("Sizga kirish 5000 so'm")
+# elif yosh <= 20:
+#     print("Sizga kirish 10000 so'm")
+# else:
+#     print("Sizga kirish 15000 so'm")
+
+# yosh = int(input("Yoshingiz nechchida?"))
+# if yosh <= 4:
+#     narh = 0
+# elif yosh <= 12:
+#     narh = 5000
+# elif yosh <= 20:
+#     narh = 1000
+# else:
+#     narh = 15000
+# print(f"Sizga kirish {narh} so'm ")
+# kun = input("Bugun qaysi kun?\n>>>")
+# if kun.lower() == 'shanba' or kun.lower() == 'yakshanba':
+#     print("Bugun dam olish kuni")
+# else:
+#     print("Bugun ish kuni")
+# kun = input("Bugun qanday kun?>>>")
+# harorat = float(input("Bugun harorat qanday?>>"))
+# if kun.lower() == 'yakshanba' and harorat >= 30:
+#     print("cho'milgani ketdik")
+# elif kun.lower() == 'Yakshanba' and harorat <= 30:
+#     print("Uyda dam olamiz")
+# taom = 15000
+# choy = False
+# salat = True
+
+# if choy and salat:
+#     taom = taom + 10000
+#     print(f"jami {taom} so'm")
+# elif choy or salat:
+#     taom = taom + 5000
+#     print(f"Jami {taom} so'm ")
+# menyu = ["manti", "somsa", "xonim", "lag'mon"]
+# ovqat = input("Nima ovqat yeysiz?\n>>>")
+# if ovqat.lower() in menyu:
+#     print(f"Buyurtmangiz qabul qilindi)")
+# else:
+#     print("Afsuski bizda bunday ovqat yo'q(")
+# menyu = ["somsa", "manti", "shashlik", "norin", "choy", "xonim"]
+# buyurtmalar = ["manti", "chuchvara", "dimlama", "choy", "somsa"]
+# for taom in buyurtmalar:
+#     if taom in menyu:
+#         print(f"Menyuda {taom} bor")
+#     else:
+#         print(f"Menyuda {taom} yo'q")
+# menyu = ["somsa", "manti", "shashlik", "norin", "choy", "xonim"]
+# ovqat = input('NIma taom tanlesiz?\n>>>')
+# if ovqat.lower() in menyu:
+#     print(f"Menyuda {ovqat} bor😊 ")
+# else:
+#     print("Bu Taom menyuda yo'q😔"
